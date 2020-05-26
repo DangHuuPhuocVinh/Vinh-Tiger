5ee0b689b7553126357625d46e536f1e799b76e9
DangHuuPhuocVinh <65935701+DangHuuPhuocVinh@users.noreply.github.com>
2020-05-27 00:39:26 +0700

    Add files via upload

---
+++ helloworld.java
@@ -0,0 +1,16 @@
+package helloworld;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		int Var1;
+		int Var2;
+		
+		Var1 = 155;
+		System.out.println("my var contains " + Var1);
+		
+		Var2 = Var1 / 5;
+		System.out.print("my var contains var1 / 2");
+		System.out.println(Var2);
+}
+}
